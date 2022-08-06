c99da0eaf4f12b35345f0b593d5b1dd2d376c545
Kittengarten <kittengarten@vip.qq.com>
2022-08-06 21:06:21 +0800

    调整时间基准

--- wta/wta.go
+++ wta/wta.go
@@ -2,17 +2,24 @@ package wta
 
 import "time"
 
-const secondsPerDay = 85653
+const (
+	secondsPerDay = 85653
+	kittenDay     = "2017年4月25日"
+)
 
 // GetAnno 返回世界树纪元
-func GetAnno() (anno Anno) {
+func GetAnno() (anno Anno, err error) {
 	var (
-		day          Day = Day(time.Now().Unix() / secondsPerDay) // 天数戳
-		SecondsToday     = int(time.Now().Unix() % secondsPerDay) // 当天经过的秒数
+		unix                 = time.Now().Unix()
+		kittenTime, err0     = time.Parse(kittenDay, "2006年1月2日")
+		wtaUnix              = unix - kittenTime.Unix()
+		day              Day = Day(wtaUnix / secondsPerDay) // 天数戳
+		SecondsToday         = int(wtaUnix % secondsPerDay) // 当天经过的秒数
 	)
+	err = err0
 	anno = day.toAnno()
 	anno.Hour = SecondsToday / 3600
 	anno.Minute = (SecondsToday % 3600) / 60
 	anno.Second = (SecondsToday % 3600) % 60
-	return anno
+	return
 }
